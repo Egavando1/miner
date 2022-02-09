151a96a46feeb19419c1f8e5bd69b15eb2233ec4
Egavando1 <99194604+Egavando1@users.noreply.github.com>
2022-02-09 19:29:35 +0700

    Create Verus.sh

---
+++ Verus.sh
@@ -0,0 +1,14 @@
+# verus
+Sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+ls
+
+tar -xvf hellminer_cpu_linux.tar.gz
+
+ls
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RX2NYHR2X5kX9zRYXUHK737gfkGabYp1ne.x01 -p hybrid --cpu 4
+
+
+
